bb52be84f05319aeb42b03c15c2b69d7fef81724
Anton Marinov <33027593+antmar0417@users.noreply.github.com>
2022-04-07 14:35:20 +0200

    Update mongo.js

--- packages/www-server/utils/mongo.js
+++ packages/www-server/utils/mongo.js
@@ -6,16 +6,8 @@ mongoose.connect(process.env.MONGODB_URI, (error) => {
   console.log("MongoDB connection successful");
 });
 
-// Add NodeJS native promises to Mongoose
-// Allows us to use async/await with Mongoose methods
 mongoose.Promise = global.Promise;
 
-/*
-    Import models - singleton,
-    allows us to do `const ModelName = mongoose.model('ModelName');`
-    in any file
-*/
-// import "../models/ModelName.js";
 import "../models/Videos.js";
 
 export { mongoose };
